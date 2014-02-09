4e14be4254946b46d0936b441de9bdbb243ae489
homyakoid <nataliya.kolesnik@gmail.com>
2014-02-09 23:31:28 +0200

    Second

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.comboBoxSource = new System.Windows.Forms.ComboBox();
+            this.comboBoxTarget = new System.Windows.Forms.ComboBox();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonCSVExport = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ratesList = new System.Windows.Forms.ListBox();
+            this.datesList = new System.Windows.Forms.ListBox();
+            this.comboBoxCurrencies = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(169, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(208, 26);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(90, 23);
+            this.buttonConvert.TabIndex = 4;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // comboBoxSource
+            // 
+            this.comboBoxSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSource.FormattingEnabled = true;
+            this.comboBoxSource.Location = new System.Drawing.Point(208, 70);
+            this.comboBoxSource.Name = "comboBoxSource";
+            this.comboBoxSource.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxSource.TabIndex = 5;
+            this.comboBoxSource.SelectedIndexChanged += new System.EventHandler(this.comboBoxSource_SelectedIndexChanged);
+            // 
+            // comboBoxTarget
+            // 
+            this.comboBoxTarget.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTarget.FormattingEnabled = true;
+            this.comboBoxTarget.Location = new System.Drawing.Point(208, 111);
+            this.comboBoxTarget.Name = "comboBoxTarget";
+            this.comboBoxTarget.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxTarget.TabIndex = 6;
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(13, 70);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(169, 20);
+            this.textBoxSource.TabIndex = 7;
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.Location = new System.Drawing.Point(13, 112);
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.ReadOnly = true;
+            this.textBoxTarget.Size = new System.Drawing.Size(169, 20);
+            this.textBoxTarget.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.buttonConvert);
+            this.groupBox1.Controls.Add(this.comboBoxSource);
+            this.groupBox1.Controls.Add(this.textBoxTarget);
+            this.groupBox1.Controls.Add(this.comboBoxTarget);
+            this.groupBox1.Controls.Add(this.textBoxSource);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 155);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "You get:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "You have:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonCSVExport);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.ratesList);
+            this.groupBox2.Controls.Add(this.datesList);
+            this.groupBox2.Controls.Add(this.comboBoxCurrencies);
+            this.groupBox2.Controls.Add(this.dateTimePicker3);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 291);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Get currencies list";
+            // 
+            // buttonCSVExport
+            // 
+            this.buttonCSVExport.Location = new System.Drawing.Point(208, 81);
+            this.buttonCSVExport.Name = "buttonCSVExport";
+            this.buttonCSVExport.Size = new System.Drawing.Size(92, 23);
+            this.buttonCSVExport.TabIndex = 11;
+            this.buttonCSVExport.Text = "Export to .csv";
+            this.buttonCSVExport.UseVisualStyleBackColor = true;
+            this.buttonCSVExport.Click += new System.EventHandler(this.buttonCSVExport_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(216, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Coose currency:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Rates list:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Dates list:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "End date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Start date:";
+            // 
+            // ratesList
+            // 
+            this.ratesList.FormattingEnabled = true;
+            this.ratesList.Location = new System.Drawing.Point(170, 130);
+            this.ratesList.Name = "ratesList";
+            this.ratesList.Size = new System.Drawing.Size(128, 147);
+            this.ratesList.TabIndex = 12;
+            // 
+            // datesList
+            // 
+            this.datesList.FormattingEnabled = true;
+            this.datesList.Location = new System.Drawing.Point(13, 130);
+            this.datesList.Name = "datesList";
+            this.datesList.Size = new System.Drawing.Size(128, 147);
+            this.datesList.TabIndex = 11;
+            this.datesList.SelectedIndexChanged += new System.EventHandler(this.datesList_SelectedIndexChanged);
+            // 
+            // comboBoxCurrencies
+            // 
+            this.comboBoxCurrencies.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrencies.FormattingEnabled = true;
+            this.comboBoxCurrencies.Location = new System.Drawing.Point(208, 38);
+            this.comboBoxCurrencies.Name = "comboBoxCurrencies";
+            this.comboBoxCurrencies.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxCurrencies.TabIndex = 9;
+            this.comboBoxCurrencies.SelectedIndexChanged += new System.EventHandler(this.comboBoxCurrencies_SelectedIndexChanged);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(13, 84);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(169, 20);
+            this.dateTimePicker3.TabIndex = 10;
+            this.dateTimePicker3.ValueChanged += new System.EventHandler(this.dateTimePicker3_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(13, 38);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(169, 20);
+            this.dateTimePicker2.TabIndex = 9;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 473);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Currency converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.ComboBox comboBoxSource;
+        private System.Windows.Forms.ComboBox comboBoxTarget;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.TextBox textBoxTarget;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBoxCurrencies;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ListBox ratesList;
+        private System.Windows.Forms.ListBox datesList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCSVExport;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+        bool formIsLoaded = false;
+        bool dj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            double resultAmount;
+            DateTime date = dateTimePicker1.Value;
+            string dateStr = date.Date.ToString("yyyy-MM-dd");
+            try
+            {
+                resultAmount = client.ConvertAmount(comboBoxSource.Text, comboBoxTarget.Text, dateStr,
+                    Convert.ToDouble(textBoxSource.Text));
+                if (resultAmount == -1)
+                    throw new KeyNotFoundException("Chosen date is not listed!");
+                textBoxTarget.Text = resultAmount.ToString();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxTarget.Text = "";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Field 'You have' must be specified in double format!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+                 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceReference1.ObjectsList ObjectsList = new ServiceReference1.ObjectsList();
+                ObjectsList = client.GetCurrencyCodes();
+                string lastDateStr = client.GetLastDate();
+                DateTime lastDate = DateTime.Parse(lastDateStr);
+                dateTimePicker2.Value = lastDate;
+                dateTimePicker3.Value = lastDate;
+                comboBoxCurrencies.Items.Add("All");
+                foreach (string id in ObjectsList.Objects)
+                {
+                    comboBoxSource.Items.Add(id);
+                    if (id != comboBoxSource.Items[0].ToString())
+                        comboBoxTarget.Items.Add(id);
+                    comboBoxCurrencies.Items.Add(id);
+                }
+                comboBoxSource.Text = comboBoxSource.Items[0].ToString();
+                comboBoxTarget.Text = comboBoxTarget.Items[0].ToString();
+                comboBoxCurrencies.Text = "All";
+                ShowRates();
+                formIsLoaded = true;
+            }
+            catch (EndpointNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void comboBoxSource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceReference1.ObjectsList ObjectsList = new ServiceReference1.ObjectsList();
+                ObjectsList = client.GetCurrencyCodes();
+                comboBoxTarget.Items.Clear();
+                foreach (string id in ObjectsList.Objects)
+                {
+                    if (id != comboBoxSource.SelectedItem.ToString())
+                        comboBoxTarget.Items.Add(id);
+                }
+                comboBoxTarget.Text = comboBoxTarget.Items[0].ToString();
+            }
+            catch (EndpointNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CheckDates(out string startDateStr, out string endDateStr)
+        {
+            DateTime startDate = dateTimePicker2.Value;
+            DateTime endDate = dateTimePicker3.Value;
+            startDateStr = startDate.Date.ToString("yyyy-MM-dd");
+            endDateStr = endDate.Date.ToString("yyyy-MM-dd");
+            if (!IfDateListed(startDateStr))
+                throw new KeyNotFoundException("Start date is not listed!");
+            if (!IfDateListed(endDateStr))
+                throw new KeyNotFoundException("End date is not listed!");
+            if (startDate > endDate)
+                throw new Exception("Start date must be less that end date!");
+        }
+
+        private void CheckComboBoxCurrencies(out string currency)
+        {
+            if (comboBoxCurrencies.Text == "All")
+            {
+                currency = null;
+                return;
+            }
+            else
+            {
+                bool exists = false;
+                ServiceReference1.ObjectsList currencyIDs = new ServiceReference1.ObjectsList();
+                currencyIDs = client.GetCurrencyCodes();
+                foreach (var id in currencyIDs.Objects)
+                {
+                    if (comboBoxCurrencies.Text == id)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (exists)
+                {
+                    currency = comboBoxCurrencies.Text;
+                    return;
+                }
+                else
+                    throw new KeyNotFoundException("There is no such currency code listed!");
+            }
+        }
+
+        private ServiceReference1.CurrencyRatesPerDateTypeList GetRates()
+        {
+            string startDateStr, endDateStr;
+            string currency;
+            CheckDates(out startDateStr, out endDateStr);
+            CheckComboBoxCurrencies(out currency);
+            return client.GetRates(startDateStr, endDateStr, currency);
+        }
+
+        private bool IfDateListed(string date)
+        {
+            ServiceReference1.ObjectsList dates = new ServiceReference1.ObjectsList();
+            dates = client.GetDates();
+            foreach (var d in dates.Objects)
+            {
+                if (d == date)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void ShowRates()
+        {
+            datesList.Items.Clear();
+            ratesList.Items.Clear();
+            try
+            {
+                ServiceReference1.CurrencyRatesPerDateTypeList objs;
+                objs = GetRates();
+                foreach (var obj in objs.RatesObj)
+                {
+                    datesList.Items.Add(obj.Date);
+                }
+            }
+            catch (KeyNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void datesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ratesList.Items.Clear();
+            try
+            {
+                ServiceReference1.CurrencyRatesPerDateTypeList objs;
+                objs = GetRates();
+                foreach (var obj in objs.RatesObj)
+                {
+                    if (datesList.SelectedItem.ToString() == obj.Date)
+                    {
+                        foreach (var rate in obj.Currencies)
+                            ratesList.Items.Add(rate);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonCSVExport_Click(object sender, EventArgs e)
+        {
+            string startDateStr, endDateStr;
+            string currency;
+            try
+            {
+                CheckDates(out startDateStr, out endDateStr);
+                CheckComboBoxCurrencies(out currency);
+                string str = client.GetStringForCSV(startDateStr, endDateStr, currency);
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "CSV file|*.csv";
+                saveFileDialog1.Title = "Save rates as CSV File";
+                saveFileDialog1.ShowDialog();
+                File.WriteAllText(saveFileDialog1.FileName, str.ToString());
+            }
+            catch (KeyNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            if (formIsLoaded)
+                ShowRates();
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
+        {
+            if (formIsLoaded)
+                ShowRates();
+        }
+
+        private void comboBoxCurrencies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (formIsLoaded)
+                ShowRates();
+        }
+    }
+}
